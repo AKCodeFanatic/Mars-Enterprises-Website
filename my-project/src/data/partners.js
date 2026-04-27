0e271fe69a48a3889be404de9f3507de87bf1c53
Akarshit <akarshitsingh1234@gmail.com>
2026-04-27 10:17:33 +0530

    Partners removed

--- my-project/src/data/partners.js
+++ my-project/src/data/partners.js
@@ -8,8 +8,8 @@ import logo6 from "../assets/logos/sf.png"
 export const partners = [
   { name: "Partner 1", logo: logo1 },
   { name: "Partner 2", logo: logo2 },
-  { name: "Partner 3", logo: logo3 },
-  { name: "Partner 4", logo: logo4 },
+  // { name: "Partner 3", logo: logo3 },
+  // { name: "Partner 4", logo: logo4 },
   { name: "Partner 5", logo: logo5 },
   { name: "Partner 6", logo: logo6 },
 ]
